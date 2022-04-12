7ebfbdb55035c911464a46a4f6cebe1a2b6b2ca6
nkunic <natasa.kunic@gmail.com>
2022-04-12 09:16:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 12-angular-example-angular-material-nk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ukywyx)
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    BrowserAnimationsModule, 
+    MaterialModule
+   ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
